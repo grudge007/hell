5fffaf579fd096c151d895f2749be7e41e60e4b2
grudge007 <77139505+grudge007@users.noreply.github.com>
2024-03-30 15:35:19 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+mv hell /usr/bin/.
+echo "moments before disaster."
+
+
+function loading_icon() {
+    local load_interval="${1}"
+    local loading_message="${2}"
+    local elapsed=0
+    local loading_animation=( '—' "\\" '|' '/' )
+
+    echo -n "${loading_message} "
+
+    # This part is to make the cursor not blink
+    # on top of the animation while it lasts
+    tput civis
+    trap "tput cnorm" EXIT
+    while [ "${load_interval}" -ne "${elapsed}" ]; do
+        for frame in "${loading_animation[@]}" ; do
+            printf "%s\b" "${frame}"
+            sleep 0.25
+        done
+        elapsed=$(( elapsed + 1 ))
+    done
+    printf " \b\n"
+}
+
+loading_icon 2 "I'm loading!"
+
+
+echo "You Are Going To Experience Hell."
+sleep 1
+python3 test.py
+
